8e62365b1705dd4c3a4451d4472a34aa479add1d
Inseob Kim <insobi@outlook.com>
2022-04-02 23:29:30 +0900

    update

--- variables.tf
+++ variables.tf
@@ -1,54 +1,64 @@
 variable "tenant" {
   type        = string
-  description = "(Required) The name of tenant"
+  description = "The name of tenant"
 }
+
 variable "vrfs" {
-  type    = map(any)
-  description = "(Optional) VRFs"
-  default = {}
+  type        = map(any)
+  description = "VRFs"
+  default     = {}
 }
+
 variable "bridge_domains" {
-  type    = map(any)
-  description = "(Optional) Bridge domains"
-  default = {}
+  type        = map(any)
+  description = "Bridge domains"
+  default     = {}
 }
+
 variable "subnets" {
-  type    = map(any)
-  description = "(Optional) Subnets of Bridge domains"
-  default = {}
+  type        = map(any)
+  description = "Subnets of Bridge domains"
+  default     = {}
 }
+
 variable "app_profiles" {
-  type    = map(any)
-  description = "(Optional) Application profiles"
-  default = {}
+  type        = map(any)
+  description = "Application profiles"
+  default     = {}
 }
+
 variable "epgs" {
-  type    = map(any)
-  description = "(Optional) Endpoint groups"
-  default = {}
+  type        = map(any)
+  description = "Endpoint groups"
+  default     = {}
 }
+
 variable "filters" {
-  type    = map(any)
-  description = "(Optional) Filters"
-  default = {}
+  type        = map(any)
+  description = "Filters"
+  default     = {}
 }
+
 variable "filter_entries" {
-  type    = map(any)
-  description = "(Optional) Filter entries"
-  default = {}
+  type        = map(any)
+  description = "Filter entries"
+  default     = {}
 }
+
 variable "contracts" {
-  type    = map(any)
-  description = "(Optional) Contracts"
-  default = {}
+  type        = map(any)
+  description = "Contracts"
+  default     = {}
 }
+
 variable "contract_bindings" {
-  type    = map(any)
-  description = "(Optional) Contract bindings"
-  default = {}
+  type        = map(any)
+  description = "Contract bindings"
+  default     = {}
 }
+
 variable "aci_domain" {
-  type    = string
-  description = "(Optional) Domain"
-  default = null
+  type        = string
+  description = "Domain"
+  default     = null
 }
